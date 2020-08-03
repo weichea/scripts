de51f990c3ef4ce16ca60c7fbc0a40a1fac8601c
weichea <13419219+weichea@users.noreply.github.com>
2020-08-03 07:48:14 -0700

    fix bug

--- mordor.py
+++ mordor.py
@@ -1,6 +1,7 @@
 import json
 import ast
 from elasticsearch import Elasticsearch, helpers
+import time
 
 
 es = Elasticsearch(
@@ -10,28 +11,35 @@ es = Elasticsearch(
     verify_certs=False,
     port=9200          
 )
+#  195,600 hits 
 
 def ingest_events(filename):
     events_dict = {}
     count = 0
-    
     with open(file, 'r') as f:
+         
         for line in f:
             count = count + 1
-            if count < 1000:
-                event_dict = {}
-                data = ast.literal_eval(line)
-            
-                if not data['SourceName'] in events_dict:
-                    events_dict.update({data['SourceName']: []})
-                    events_dict[data['SourceName']].append(data)
-                else:
-                    events_dict[data['SourceName']].append(data)
-            else:
+            data = ast.literal_eval(line)
 
+            
+            if not data['SourceName'] in events_dict:
+                events_dict.update({data['SourceName']: []})
+                events_dict[data['SourceName']].append(data)
+            else:
+                events_dict[data['SourceName']].append(data)
+
+            if count >= 1000:
                 es_bulk_insert(events_dict)
                 count = 0
                 events_dict = {}
+
+
+        if events_dict:
+            es_bulk_insert(events_dict)
+
+            
+        
 
 def es_bulk_insert(events_dict):
     for k in events_dict.keys():
@@ -39,13 +47,14 @@ def es_bulk_insert(events_dict):
             new_k = '-'.join(k.split())
         else:
             new_k = k
+
         helpers.bulk(
             es, 
             events_dict[k],
-            stats_only=True,
             index='mordor-' + new_k.lower()
     )
 
-for file in ['./datasets/large/apt29/day1/apt29_evals_day1_manual_2020-05-01225525.json',\
-     './datasets/large/apt29/day2/apt29_evals_day2_manual_2020-05-02035409.json']:
+for file in ['./apt29_evals_day1_manual_2020-05-01225525.json', './apt29_evals_day2_manual_2020-05-02035409.json']:
     events_dict = ingest_events(file)
+
+
